12a6ae49076f02a837ba5241f0b2f2638b2e9f5a
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-09-13 12:04:43 +0100

    debugging Node.js

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { label } from './App';
 
-test('renders learn react link', () => {
+test('render hello world', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('generates a label', () => {
+  const result = label('React');
+  expect(result).toEqual('Hello REACT');
+})
